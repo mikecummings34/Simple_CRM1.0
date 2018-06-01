61e9ace3188559106da3aa3ef2f6e5f54d995b89
Mike <mikecummings221@gmail.com>
2018-06-01 00:22:59 -0600

    client custom views working

--- crm_app/urls.py
+++ crm_app/urls.py
@@ -27,10 +27,18 @@ urlpatterns = [
 	url(r'^tickets/new-entry/$', views.new_entry, name='new_entry'),
     url(r'^tickets/new/$', views.create_ticket, name='create_ticket'),
     url(r'^tickets/$', views.tickets, name='tickets'),
-    url(r'^client/$', views.client, name='client'),
-    url(r'^clienttest/$',  views.clienttest.as_view(), name = "clienttest"),
-    url(r'^client_detail/$',  views.client_detail.as_view(), name = "client_detail"),
-    	
+
+
+
+
+    url(r'^client/$', views.ClientGet.as_view(), name='client'),
+    url(r'^clientq/$', views.ClientPostQ.as_view(), name = 'client_detail'),
+    url(r'^client_tech/$', views.ClientGetTech.as_view(), name = 'client_tech'),
+    url(r'^client_tickets/$', views.ClientPostTickets.as_view(), name = 'client_tickets'),
+
+    #url(r'^clienttest/$',  views.clienttest.as_view(), name = "clienttest"),	
+    #url(r'^clientposttest/$',  views.clientposttest.as_view(), name = "clientposttest"),
+    
 
 
 
--- crm_app/views.py
+++ crm_app/views.py
@@ -8,10 +8,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.forms import modelform_factory
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.db.models import F
 from django.views.generic import ListView, FormView, View, DetailView
 from django.shortcuts import get_object_or_404
+
 
 
 # Create your views here.
@@ -27,36 +29,12 @@ def view_profile(request):
 		args = {'user': request.user}
 		return render(request, 'crm_temps/view-profile.html', args)
 """
+
+
+
+
 ##class based view of view profile
 
-class view_profile(ListView):
-
-	template_name = 'crm_temps/view-profile.html'
-	context_object_name = 'placeholder'
-	http_method_names = ['get','post']
-	
-	def get_context_data(self, **kwargs):
-		context = super(view_profile, self).get_context_data(**kwargs)
-		if self.request.method == "POST":
-			context['user'] =  self.request.user
-			context['tickets'] = Servicetickets.objects.filter(technician = self.request.POST['recent_tickets']).order_by('-ticketstartdate')
-			return context
-		else:
-			context['user'] = self.request.user
-			return context
-
-	def get_queryset(self):
-		if self.queryset is None:
-			return self.queryset
-
-	def post(self, request, *args, **kwargs):
-		if self.request.method == "POST":
-			self.object_list = self.get_queryset()
-			allow_empty = self.get_allow_empty()
-			context = self.get_context_data()
-			return self.render_to_response(context)
-		else:
-			HttpResponse("fuck you")
 '''
 def ticket_detail(request):
 	if request.method == "GET":
@@ -68,18 +46,7 @@ def ticket_detail(request):
 			return render(request, 'crm_temps/ticket-details.html', lib)
 '''
 
-class ticket_detail(ListView):
-	template_name = 'crm_temps/ticket-details.html'
-	context_object_name = 'placeholder'
 
-	def get_context_data(self, **kwargs):
-		context = super(ticket_detail, self).get_context_data(**kwargs)
-		context['tickets'] =  Servicetickets.objects.get(oid = self.request.GET['ticket'])
-		context['entries'] = Timeentries.objects.filter(ticketid = self.request.GET['ticket'])
-		return context
-	def get_queryset(self):
-		if self.queryset is None:
-			return self.queryset
 
 
 def new_entry(request):
@@ -134,24 +101,24 @@ def tickets(request):
 			q = request.GET['cid']
 			tickets = Servicetickets.objects.filter(clientid=q)
 			lib = {'tickets':tickets}
-			return render(request, 'crm/tickets.html', lib)
+			return render(request, 'crm_temps/tickets.html', lib)
 		elif 'tech' in request.GET and request.GET['tech']:
 			q=request.GET['tech']
 			tickets = Servicetickets.objects.filter(technician=q)
 			lib = {'tickets':tickets}
-			return render(request, 'crm/tickets.html', lib)
+			return render(request, 'crm_temps/tickets.html', lib)
 		elif 'filter' in request.GET and request.GET['filter']:
 			q = request.GET['filter']
 			tickets = Servicetickets.objects.filter(ticket_status=1)
 			lib={'tickets':tickets}
-			return render(request, 'crm/tickets.html')
+			return render(request, 'crm_temps/tickets.html')
 
 		else:	
 			tickets = Servicetickets.objects.all().order_by('-ticketstartdate')[:30]
 			lib = {'tickets': tickets}
-			return render(request, 'crm/tickets.html', lib)
+			return render(request, 'crm_temps/tickets.html', lib)
 
-
+'''
 def client(request):
 	if request.method == "POST":
 		if 'q' in request.POST and request.POST['q']: 
@@ -182,37 +149,44 @@ def client(request):
 				prnt = print(request)
 				value = {'client': query}
 				return render(request, 'crm_temps/client.html', value)
-
+'''
 
 ###this could work, but need to render different templates for each request type.
-
-class clienttest(ListView):
-	model = Clientlist
-	query_set = Clientlist.objects.all()
-	template_name = 'crm_temps/client.html'
-	context_object_name = 'client'
+'''
+class client(ListView):
+	#model = Clientlist
+	#query_set = Clientlist.objects.all()
+	#template_name = []
+	#context_object_name = 'client'
 
 	def get_context_data(self, **kwargs):
-		context = super(clientposttest, self).get_context_data(**kwargs)
+		context = super(client, self).get_context_data(**kwargs)
 		if self.request.method == 'GET':
 			if self.request.GET == 'tech':
 				context['tickets'] = Servicetickets.objects.filter(technician = self.request.GET['tech'])
 				return context
 			else:
+				context['client'] = Clientlist.objects.all().values()
 				return context
-		elif self.request.method == "POST":
+		if self.request.method == "POST":
 			if self.request.POST == 'q':
 				context['clientinfo'] = Clientlist.objects.get(oid = self.request.POST['q'])
 				context['contact'] = Contacts.objects.filter(clientid = self.request.POST['q'])
 				return context
-			elif self.request.POST == 'tickets':
+			if self.request.POST == 'tickets':
 				context['tickets'] = Servicetickets.objects.filter(clientid = self.request.POST['tickets'])
 				context['name'] = Clientlist.objects.get(oid = self.request.POST['tickets'])
 				return context 
 
 	def get_queryset(self):
 		if self.queryset is None:
-			return self.queryset
+			if self.request.method == "GET":
+				self.queryset = Clientlist.objects.all()
+				return self.queryset
+			if self.request.method == "POST":
+				if 'q' in self.request.POST and self.request.POST['q']:
+					self.queryset
+
 
 	def post(self, request, *args, **kwargs):
 		if self.request.method == "POST":
@@ -220,12 +194,26 @@ class clienttest(ListView):
 			allow_empty = self.get_allow_empty()
 			context = self.get_context_data()
 			return self.render_to_response(context)
-		else:
-			HttpResponse("fuck you")		
+
+	def get_template_names(self):
+		self.template_name ='crm_temps/client.html'
+		#template_name = super(client, self).get_template_names()
+		req = self.request
+		if req.method == "GET":
+			template_name = 'crm_temps/client.html'
+			return self.template_name
+		if self.request.method == "POST":
+			if 'q' in self.request.POST and self.request.POST['q']:
+				self.template_name = 'crm_temps/client_profile.html'
+				return super(client, self).get_template_names()
+
+
+'''
 
 
 
 
+'''
 class client_detail(ListView):
 	#query_set = Clientlist.objects.all()
 	template_name = 'crm_temps/client_profile.html'
@@ -239,14 +227,111 @@ class client_detail(ListView):
 		context['contact'] =  Contacts.objects.filter(clientid = self.request.GET['q'])
 		context['clientinfo'] = Clientlist.objects.get(oid = self.request.GET['q'])
 		return context
+
+	def get_queryset(self):
+		if self.queryset is None:
+			return self.queryset
+'''
+
+
+
+
+
+## Client Functions in CBV START HERE##
+######################################
+#####################################
+####################################
+class ClientMixin(object):
+	"""Main mixin for client based functions. Change this before live. failed on get is an option so resubmitting
+	a posted page will redirect to post it again"""
+	failed_on_get = False
+	def get_queryset(self, **kwargs):
+		if self.queryset is None:
+			return self.queryset
+
+	def post(self, *args, **kwargs):
+		if self.request.method == "POST" or self.failed_on_get == 'True':
+			self.object_list = self.get_queryset()
+			allow_empty = self.get_allow_empty()
+			context = self.get_context_data()
+			return self.render_to_response(context)
+
+##vvvvvCBVs that Inherit from Client Mixin vvvvvvv###
+class view_profile(ClientMixin, ListView):
+
+	template_name = 'crm_temps/view-profile.html'
+	context_object_name = 'placeholder'
+	http_method_names = ['get','post']
+	
+	def get_context_data(self, **kwargs):
+		context = super(view_profile, self).get_context_data(**kwargs)
+		if self.request.method == "POST":
+			context['user'] =  self.request.user
+			context['tickets'] = Servicetickets.objects.filter(technician = self.request.POST['recent_tickets']).order_by('-ticketstartdate')
+			return context
+		else:
+			context['user'] = self.request.user
+			return context
+
 	def get_queryset(self):
 		if self.queryset is None:
 			return self.queryset
 
+	def post(self, request, *args, **kwargs):
+		if self.request.method == "POST":
+			self.object_list = self.get_queryset()
+			allow_empty = self.get_allow_empty()
+			context = self.get_context_data()
+			return self.render_to_response(context)
+		else:
+			HttpResponse("fuck you")
+
+class ticket_detail(ClientMixin, ListView):
+	template_name = 'crm_temps/ticket-details.html'
+	context_object_name = 'placeholder'
+
+	def get_context_data(self, **kwargs):
+		context = super(ticket_detail, self).get_context_data(**kwargs)
+		context['tickets'] =  Servicetickets.objects.get(oid = self.request.GET['ticket'])
+		context['entries'] = Timeentries.objects.filter(ticketid = self.request.GET['ticket'])
+		return context
+
+class ClientPostQ(ClientMixin, ListView):
+	template_name = "crm_temps/client_profile.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(ClientPostQ, self).get_context_data(**kwargs)
+		context['clientinfo'] = Clientlist.objects.get(oid = self.request.POST['q'])
+		context['contact'] = Contacts.objects.filter(clientid = self.request.POST['q'])
+		return context
+	def get(self, request, *args, **kwargs):
+		return HttpResponseRedirect(reverse('crm:client'))
+
+class ClientPostTickets(ClientMixin, ListView):
+	template_name = "crm_temps/tickets.html"
+
+	def get_context_data(self, **kwargs):
+		context['tickets'] = Servicetickets.objects.filter(clientid=self.request.POST['tickets']).order_by("-timestamp")
+		context['name'] = Clientlist.objects.get(oid = self.request.POST['tickets'])
+		return context
+
+class ClientGetTech(ClientMixin, ListView):
+	template_name = "crm_temps/ticket-detail.html"
+
+	def get_context_data(self, **kwargs):
+		context['tech'] = Servicetickets.objects.filter(technician=self.request.POST['tech'])
+		return context
 
 
+class ClientGet(ListView):
+	template_name = "crm_temps/client.html"
+	model = 'Clientlist'
+	queryset = Clientlist.objects.all()
+	context_object_name = 'client'
 
-	
+####^^^^^^^End of ClientMixin CBVs^^^^^^^^^^########
+##################################################
+#####################################################
 
 	
 
